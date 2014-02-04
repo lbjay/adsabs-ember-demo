19be4b52128e1403de7b7daafc8d025135ff6ee8
Jay Luker <jluker@cfa.harvard.edu>
2014-02-04 17:05:35 -0500

    MOAR CHANGEZ!

--- static/js/app/app.js
+++ static/js/app/app.js
@@ -10,5 +10,93 @@
  * 6. list of search results is sortable
 */
 
-window.App = Ember.Application.create();
-App.ApplicationAdapter = DS.FixtureAdapter.extend();
+window.Search = Ember.Application.create();
+Search.ApplicationAdapter = DS.FixtureAdapter.extend();
+
+Search.Result = Ember.Object.extend({
+	bibcode: null,
+	title: null,
+	abs: null,
+	isSelected: false
+});
+
+/* routers */
+
+Search.Router.map(function() {
+  this.route('search', { path: '/' });
+});
+
+Search.SearchController = Ember.ObjectController.extend({
+	userQuery: '',
+	rows: 20,
+	actions: {
+		executeSearch: function() {
+			console.log('foo! ' + this.userQuery);
+		}
+	}
+});
+
+//Search.SearchRoute = Ember.Route.extend({
+//	model: function() {
+//		return this.store.find('searchresult');
+//	}
+//});
+//
+///* controllers */
+//
+//App.SearchController = Ember.ArrayController.extend({
+//	appName: 'Ember Search Demo',
+//	actions: {
+//		updateResultList: function() {
+//			// Get the query value
+//			var query = this.get('newQuery');
+//			if (!query.trim()) { return; }
+//
+//			// Create the new Todo model
+//			var todo = this.store.createRecord('todo', {
+//				title: title,
+//				isCompleted: false
+//			});
+//
+//			// Clear the "New Todo" text field
+//			this.set('newTitle', '');
+//
+//			// Save the new model
+//			todo.save();
+//		}
+//	}
+//});
+//
+//App.QueryController = Ember.ObjectController.extend({
+//	q: '',
+//	rows: 20
+//});
+//
+//App.SearchResultController = Ember.ArrayController.extend({
+//	
+//});
+//
+///* models */
+//
+//App.SearchResultList = DS.Model.extend({
+//	results: DS.hasMany('searchresult')
+//});
+//
+//App.SearchResult = DS.Model.extend({
+//  title: DS.attr('string'),
+//  abs: DS.attr('string'),
+//  isSelected: DS.attr('boolean'),
+//  resultList: DS.belongsTo('searchresultlist')
+//});
+//
+//App.Facet = DS.Model.extend({
+//	field: DS.attr('string'),
+//	isOpen: DS.attr('boolean'),
+//	facetValues: DS.hasMany('facetValue')
+//})
+//
+//App.FacetValue = DS.Model.extend({
+//	label: DS.attr('string'),
+//	count: DS.attr('int'),
+//	facet: DS.belongsTo('facet')
+//})
